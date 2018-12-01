4a2062502bd70c57ce08565f0af6d63dd1d01afd
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2018-11-30 21:57:15 -0600

    Prevent dangling references of Revision objects
    IDocument must act as one big object. Since Revision objects hold a
    circular reference to IVersionController, holding a reference to it
    means also holding its IVersionController. This commit prevents others
    from hodling references to Revision objects; others can only access
    Revision objects for a limited time.

--- MapMaker/DocumentModel/Revision.cs
+++ MapMaker/DocumentModel/Revision.cs
@@ -1,9 +1,9 @@
 ﻿
 namespace MapMaker.DocumentModel
 {
-    public class Revision<T>
+    public class Revision<T> where T : struct
     {
-        internal T _currentValue;
+        private T _currentValue;
         private readonly IVersionController _versionController;
 
         public Revision(IVersionController versionController)
@@ -11,13 +11,40 @@ namespace MapMaker.DocumentModel
             _versionController = versionController;
         }
 
-        public static implicit operator T(Revision<T> revision) => revision._currentValue;
+        public static implicit operator View(Revision<T> revision) => new View(revision);
 
-        public void CheckIn(T newValue)
+        public ref struct View
         {
-            _versionController.ExecuteCommand(
-                new CheckInCommand<T>(this, _currentValue, newValue)
-            );
+            private readonly Revision<T> _target;
+
+            public View(Revision<T> target) => _target = target;
+
+            public static implicit operator T(View view) => view._target._currentValue;
+
+            public void CheckIn(T newValue)
+            {
+                _target._versionController.ExecuteCommand(
+                    new CheckInCommand(_target, _target._currentValue, newValue)
+                );
+            }
+        }
+
+        private sealed class CheckInCommand : ICommand
+        {
+            private readonly Revision<T> _property;
+            private readonly T _oldValue;
+            private readonly T _newValue;
+
+            public CheckInCommand(Revision<T> property, T oldValue, T newValue)
+            {
+                _property = property;
+                _oldValue = oldValue;
+                _newValue = newValue;
+            }
+
+            public void Execute() => _property._currentValue = _newValue;
+
+            public void Undo() => _property._currentValue = _oldValue;
         }
     }
 }
